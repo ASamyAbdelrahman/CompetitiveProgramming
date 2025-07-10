0fbf0b33c0c42a5029cc614a787c6280d055222d
Abdelrahman Samy <abdelrahmansamy522@gmail.com>
2025-07-10 21:59:55 +0300

    Get the sum of all divisors of an integer n

---
+++ Al_Azhar_Sheets/level_1/week_2/B_Summation_of_its_divisors.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+/**
+ * @brief Calculates the sum of all divisors of a given integer n.
+ *
+ * This function iterates from 1 to sqrt(n), and for each divisor i of n,
+ * it adds both i and n/i to the sum (taking care not to double-count when i == n/i).
+ *
+ * @param n The integer whose divisors' sum is to be calculated.
+ * @return The sum of all divisors of n.
+ */
+int getSumofDivisors(int n) {
+    int sum = 0;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            sum += i;
+            if (i != n / i) {
+                sum += n / i;
+            }
+        }
+    }
+
+    return sum;
+}
+
+void solve(void) {
+    int n;
+    cin >> n;
+    int sum = getSumofDivisors(n);
+    cout << sum << endl;
+}
+
+int main(void) {
+    fast_io
+
+    solve();
+
+    return 0;
+}
